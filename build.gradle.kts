de87ace2c791ff981d51b7936fe480e98dba6121
LossyDragon <thedragon1012@gmail.com>
2023-04-27 16:35:30 -0500

    Move gradle to kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+}
+
+plugins {
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.google.dagger.hilt.android") version "2.45" apply false
+}
+
+// Used to try not to cache JavaSteam snapshots when developing
+configurations.configureEach {
+    resolutionStrategy.cacheChangingModulesFor(0, "seconds")
+}
